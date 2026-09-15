819a604155488f5428c916d71c6ff7171334e6e9
wnn <273213185@qq.com>
2026-09-15 22:23:09 +0800

    finish

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ from shenwu.controller import GameController
 if __name__ == '__main__':
     game_controller = GameController()
 
-    # while True:
-    #     try:
-    #         game_controller.run_xun_you()
-    #         time.sleep(game_controller.human_delay(600, sigma=0.15))
-    #     except Exception as e:
-    #         print(e)
-
     while True:
-        game_controller.run_fish()
-        time.sleep(game_controller.human_delay(3, sigma=0.15))
+        try:
+            game_controller.run_fish()
+            time.sleep(game_controller.human_delay(5, sigma=0.15))
+        except Exception as e:
+            print(e)

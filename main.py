962ffcbb00b0068c3aa72f9474096bed6c460529
wnn <273213185@qq.com>
2026-09-17 08:43:35 +0800

    finish

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 
     while True:
         try:
-            game_controller.run_xun_you()
-            time.sleep(game_controller.human_delay(3, sigma=0.15))
+            game_controller.run_auto_reset_round()
+            time.sleep(game_controller.human_delay(10, sigma=0.15))
         except Exception as e:
             print(e)
